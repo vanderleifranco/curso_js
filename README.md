4889cec74df197de0fa9d94f374cc31d72824654
vanderleifranco <profvanderfanco@gmail.com>
2025-03-23 10:39:31 -0300

    aula 01-vs01

---
+++ README.md
@@ -0,0 +1,11 @@
+# curso_2025
+Curso 2025
+
+Curso -- Adelia
+
+[![N|Solid](https://images.pexels.com/photos/163157/mario-luigi-figures-funny-163157.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260)](https://github.com/vanderfranco/)
+
+
+
+#Curso -- 
+AULA 01 - executando um script e entendo o lado cliente. 
---
+++ script.js
@@ -0,0 +1,23 @@
+/**
+ * Diferença entre var, let e const:
+ * 
+ * - `var`: É uma forma mais antiga de declarar variáveis no JavaScript. 
+ *   Possui escopo de função (não de bloco) e pode ser redeclarada no mesmo escopo.
+ *   Além disso, variáveis declaradas com `var` são "hoisted" (elevadas) para o topo do escopo,
+ *   mas sem inicialização, o que pode levar a comportamentos inesperados.
+ * 
+ * - `let`: Introduzido no ES6, possui escopo de bloco, o que significa que a variável só é acessível
+ *   dentro do bloco onde foi declarada. Não pode ser redeclarada no mesmo escopo, mas pode ser reatribuída.
+ * 
+ * - `const`: Também introduzido no ES6, possui escopo de bloco como `let`. 
+ *   No entanto, uma variável declarada com `const` não pode ser reatribuída após sua inicialização.
+ *   É usada para valores que não devem mudar, mas isso não impede a mutação de objetos ou arrays.
+ */
+
+// numero pi
+const PI = 3.14159;
+console.log(PI);
+
+// erro: tentando reatribuir um valor a uma constante
+PI = 3.14;
+console.log(PI);
